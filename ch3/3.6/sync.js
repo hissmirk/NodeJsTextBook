1809abf35cde11f990488961a156fe5d9c372011
hissmirk <false9@kakao.com>
2022-08-31 16:47:23 +0900

    3.6.1 동기 메서드와 비동기 메서드

---
+++ ch3/3.6/sync.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+console.log('시작');
+// 메서드 readFile 대신 readFileSync 사용
+let data = fs.readFileSync('./readme2.txt');
+console.log('1번', data.toString());
+data = fs.readFileSync('./readme2.txt');
+console.log('2번', data.toString());
+data = fs.readFileSync('./readme2.txt');
+console.log('3번', data.toString());
+console.log('끝');
