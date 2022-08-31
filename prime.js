6c11af19d315a0d5b031a65e5cf4fc99ac0c4b8c
hissmirk <false9@kakao.com>
2022-08-31 10:45:33 +0900

    3.5.7 worker_threads

---
+++ prime.js
@@ -0,0 +1,21 @@
+// const min = 2;
+// const max = 10000000;
+// const primes = [];
+//
+// function generatePrimes(start, range) {
+//     let isPrime = true;
+//     const end = start + range;
+//     for (let i = start; i < end; i++) {
+//         for (let j = min; j < Math.sqrt(end); j++) {
+//
+//         }
+//
+//     }
+// }
+//
+// console.time('prime');
+// generatePrimes(min, max);
+// console.timeEnd('prime');
+// console.log(primes.length);
+
+console.log(Math.sqrt(100));
